e64141fc8e04a877ea791ce35e8f689f83396141
Yechan Lee <yechan24680@gmail.com>
2022-05-20 15:26:56 +0900

    add Post create, update feature

--- src/main/java/com/yechan2468/springboot/service/posts/PostsService.java
+++ src/main/java/com/yechan2468/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.yechan2468.springboot.service.posts;
 
+import com.yechan2468.springboot.domain.posts.Posts;
 import com.yechan2468.springboot.domain.posts.PostsRepository;
+import com.yechan2468.springboot.web.dto.PostsResponseDto;
 import com.yechan2468.springboot.web.dto.PostsSaveRequestDto;
+import com.yechan2468.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,21 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        return new PostsResponseDto(entity);
     }
 }
---
+++ src/main/java/com/yechan2468/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.yechan2468.springboot.web.dto;
+
+import com.yechan2468.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}

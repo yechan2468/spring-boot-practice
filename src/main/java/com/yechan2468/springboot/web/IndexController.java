47f5b84fafb803ae94981491540846a8cbd42efd
Yechan Lee <yechan24680@gmail.com>
2022-05-31 14:09:18 +0900

    refactor 로그인 세션 가져오기

--- src/main/java/com/yechan2468/springboot/web/IndexController.java
+++ src/main/java/com/yechan2468/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.yechan2468.springboot.web;
 
+import com.yechan2468.springboot.config.auth.LoginUser;
 import com.yechan2468.springboot.config.auth.dto.SessionUser;
 import com.yechan2468.springboot.service.posts.PostsService;
 import com.yechan2468.springboot.web.dto.PostsResponseDto;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -19,10 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user= (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
